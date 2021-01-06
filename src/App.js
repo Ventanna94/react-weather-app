a120078bac927893f2fe571795a1cf3ae0ad9bd0
Anna V <71232941+Ventanna94@users.noreply.github.com>
2021-01-06 10:50:52 +0000

    Removed Bootstrap to add it from npm

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1> Weather App </h1>
       <footer>
         Lovingly coded by Anna V and 
       <a href="https://github.com/Ventanna94/react-weather-app" target="_blank"> open-sourced </a>
       </footer>
+      </div>
     </div>
   );
 }
