de43c131ced4168e53069c9376622cb139ae3f11
Anna V <71232941+Ventanna94@users.noreply.github.com>
2021-01-06 10:31:07 +0000

    Added link to GitHub repository

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ export default function App() {
   return (
     <div className="App">
       <h1> Weather App </h1>
+      <footer>
+        Lovingly coded by Anna V and 
+      <a href="https://github.com/Ventanna94/react-weather-app" target="_blank"> open-sourced </a>
+      </footer>
     </div>
   );
 }
