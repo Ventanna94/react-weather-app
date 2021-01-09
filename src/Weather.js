268dcf66abfa34fd45c1a2c27cd20605bda41575
Anna V <71232941+Ventanna94@users.noreply.github.com>
2021-01-09 18:45:24 +0000

    Added API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,27 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import "./Weather.css";
+import "./index.css";
 
-export default function Weather() {
-    return (
+export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({loaded: false});
+    function handleResponse(response) {
+    setWeatherData({
+        loaded: true,
+        temperature: Math.round(response.data.main.temp),
+        city: response.data.name,
+        date: "09/01",
+        time: "19:33",
+        description: response.data.weather[0].description,
+        iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+        wind: Math.round(response.data.wind.speed),
+        humidity: response.data.main.humidity,
+        feelsLike: Math.round(response.data.main.feels_like)
+    });    
+    }
+
+    if (weatherData.loaded) {
+        return (
         <div className="Weather"> 
-        <form>
-        <input type="search" placeholder="Type a city" className="form-control"/>
+        <form className="searchForm">
+        <input type="search" placeholder="Take me to..." className="form-control"autoFocus="on" />
         <input type="submit" value="Search" className="btn btn-primary" />
-        </form>
-        <h2> Milan </h2>
-        <h4> 06/01 | 11:00 </h4>
-        <h4> Fog </h4>
-        <div className="row">
-            <div className="col-6"> 
-            <img src="https://ssl.gstatic.com/onebox/weather/64/fog.png" /> 
-            5°C
-            </div>
-            <div className="col-6">
-             <h4> Humidity: 90% </h4>
-             <h4> Wind: 12 km/h </h4>   
-            </div>
+        <input type="submit" value="Current" className="btn btn-info" />
+        <input type="submit" value="Get Inspired" className="btn btn-outline-info" />
 
+        </form>
+        <div className="row">
+         <div className="col-6">
+         <h1 className="city"> {weatherData.city} </h1>
+         <h2 className="date"> {weatherData.date} | {weatherData.time} </h2>
+         <div className="clearfix"> <img src={weatherData.iconUrl} className="float-left" /> </div>
+         <h4 className="temperature"> {weatherData.temperature}° C | <span> Feels like {weatherData.feelsLike}° C </span> </h4>
+         <h5 className="text-capitalize"> {weatherData.description} </h5>
+         </div>
+    
+         <div className="col-6">
+         <h4 className="humidity"> Humidity: {weatherData.humidity}% </h4>
+         <h4 className="wind"> Wind: {weatherData.wind} km/h </h4>   
+         </div>
         </div>
         </div>
     );
+    } else {
+        const apiKey= "2dce67821f0e7f4647f02add3197ae70";
+        let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+        return ("Loading...");
+    }
 }
+    
